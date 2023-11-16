cc931f1388e3e68e44e93203c7fb9360e46f01ed
Yaqi Fang <cfang322@gmail.com>
2023-11-16 15:28:49 -0500

    finished enrollment file

---
+++ app/models/enrollment.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Enrollment < ApplicationRecord
+    has_many :students, 
+        primary_key: :id,        #user.id
+        foreign_key: :student_id,#enrollment.student_id 
+        class_name: :User
+    has_many :course,
+        primary_key: :id,        #course.id 
+        foreign_key: :course_id, #enrollment.course_id 
+        class_name: 'Course'
+end
